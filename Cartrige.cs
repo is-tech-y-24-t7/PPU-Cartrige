bccea9474632b9f5135cc387fc3fcf288a3fbe20
noidandroid <48490323+noidandroid@users.noreply.github.com>
2022-06-08 23:44:09 +0300

    initial: basic NES cartidge bytefield implemented

---
+++ Cartrige.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace GraphicProcessingUnit
+{
+    public class Cartridge
+    {
+        const int HeaderMagic = 0x1A53454E;
+
+        byte[] _prgRom;
+        byte[] _chr;
+        byte[] _prgRam;
+        public Console Console { get; set; }
+        public int PrgRomBanks { get; private set; }
+        public int ChrBanks { get; private set; }
+        public bool VerticalVramMirroring { get; private set; }
+        public bool BatteryBackedMemory { get; private set; }
+        public bool ContainsTrainer { get; private set; }
+        public bool UsesChrRam { get; private set; }
+        public int MapperNumber { get; private set; }
+        public bool Invalid { get; private set; }
+
+        int _flags6;
+        int _flags7;
+        
+        public Cartridge(string path)
+        {
+            
+        }
+        
+    }   
+}
