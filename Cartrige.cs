ed819d7fb2d9297773ac0fcb3a7ce683dd2d33bd
lievvl <azazinba@yandex.ru>
2022-06-09 23:31:33 +0300

    feat: done ParseHeader

--- Cartrige.cs
+++ Cartrige.cs
@@ -5,11 +5,11 @@ namespace GraphicProcessingUnit
 {
     public class Cartridge
     {
-        const int HeaderMagic = 0x1A53454E;
+        private const int Signature = 0x1A53454E;
 
-        byte[] _prgRom;
-        byte[] _chr;
-        byte[] _prgRam;
+        private byte[] _prgRom;
+        private byte[] _chr;
+        private byte[] _prgRam;
         public Console Console { get; set; }
         public int PrgRomBanks { get; private set; }
         public int ChrBanks { get; private set; }
@@ -20,8 +20,7 @@ namespace GraphicProcessingUnit
         public int MapperNumber { get; private set; }
         public bool Invalid { get; private set; }
 
-        int _flags6;
-        int _flags7;
+        private int _flags6;
         
         public Cartridge(string path)
         {
@@ -86,6 +85,12 @@ namespace GraphicProcessingUnit
 
         void ParseHeader(BinaryReader reader)
         {
+            // 0-3 сигнатура файла
+            // 4 размер PRG ROM
+            // 5 Размер CHR ROM (или 0 если CHR RAM)
+            // 6 - флаг на маппер и вертикальный/горизонатальный мирроринг
+            // 7 и более - не используем (считывать NES2 не будем)
+
             uint Num = reader.ReadUInt32();
             if (Num != HeaderMagic)
             {
@@ -93,7 +98,40 @@ namespace GraphicProcessingUnit
                 Invalid = true;
                 return;
             }
-            // допилить
+
+            PrgRomBanks = reader.ReadByte();
+            System.Console.WriteLine("PRG ROM = " + (16 * PrgRomBanks).ToString() + " Kb");
+
+            ChrBanks = reader.ReadByte();
+            if (ChrBanks == 0)
+            {
+                System.Console.WriteLine("Использование CHR RAM");
+                ChrBanks = 2;
+                UsesChrRam = true;
+            }
+            else
+            {
+                System.Console.WriteLine((8 * ChrBanks).ToString() + "Kb of CHR ROM");
+                UsesChrRam = false;
+            }
+
+            // 0 бит тип mirroring
+            // 1 бит содержит ли battery-backed PRG RAM
+            // 2 бит есть ли трейнер
+            // 3 бит пока-что не используем (вертикально-горизонтальный mirroring)
+            _flags6 = reader.ReadByte();
+            VerticalVramMirroring = ((_flags6 & 0b00000001) != 0);
+            System.Console.WriteLine("VRAM mirroring type: " + (VerticalVramMirroring ? "vertical" : "horizontal"));
+            if (VerticalVramMirroring)
+                System.Console.WriteLine("Вертикальное отражение");
+            else
+                System.Console.WriteLine("Горизонтальное отражение")
+
+            BatteryBackedMemory = (_flags6 & 0x02) != 0;
+            ContainsTrainer = (_flags6 & 0x04) != 0;
+
+            //TODO: в будущем реализовать считывание 4-х экранного отражения
+            //TODO: реализовать считывание редко-используемых полей (байты 7-10)
         }
     }   
 }
