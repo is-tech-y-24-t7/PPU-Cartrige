4903d4ce59eec11ca0f54e07bd8a9a8253e38dd7
Maksss_ssskaM <73906341+1MakssskaM1@users.noreply.github.com>
2022-06-12 02:13:50 +0300

    feat: ReadFromRegister, WriteToRegister

--- Registers.cs
+++ Registers.cs
@@ -4,6 +4,9 @@ namespace GraphicProcessingUnit
     {
         readonly PpuMemory _memory;
         readonly Console _console;
+        byte[] _oam;
+        ushort _oamAddr;
+        int _vRamIncrement;
         ushort v;
         ushort t;
         byte x;
@@ -12,78 +15,125 @@ namespace GraphicProcessingUnit
 
         public byte ReadFromRegister(ushort address)
         {
+            byte data;
+            switch (address)
+            {
+                case 0x2002:
+                    data = ReadPpuStatus();
+                    break;
+                case 0x2004:
+                    data = ReadOamData();
+                    break;
+                case 0x2007:
+                    data = ReadPpuData();
+                    break;
+                default:
+                    throw new Exception("Неверный регистр PPU был считан из регистра: " + address.ToString("X4"));
+            }
 
+            return data;
         }
 
         public void WriteToRegister(ushort address, byte data)
         {
-
+            _lastRegisterWrite = data;
+            switch (address)
+            {
+                case 0x2000:
+                    WritePpuCtrl(data);
+                    break;
+                case 0x2001:
+                    WritePpuMask(data);
+                    break;
+                case 0x2003:
+                    WriteOamAddr(data);
+                    break;
+                case 0x2004:
+                    WriteOamData(data);
+                    break;
+                case 0x2005:
+                    WritePpuScroll(data);
+                    break;
+                case 0x2006:
+                    WritePpuAddr(data);
+                    break;
+                case 0x2007:
+                    WritePpuData(data);
+                    break;
+                case 0x4014:
+                    WriteOamDma(data);
+                    break;
+                default:
+                    throw new Exception("Неверный регистр PPU был записан в регистр: " + address.ToString("X4"));
+            }
         }
 
         // $2000
         public void WritePpuCtrl(byte data)
         {
-
+            // допилить
         }
 
         // $2001
         public void WritePpuMask(byte data)
         {
-
-        }
-
-        // $4014
-        public void WriteOamAddr(byte data)
-        {
-
-        }
-
-        // $2004
-        public void WriteOamData(byte data)
-        {
-
-        }
-
-        // $2005
-        public void WritePpuScroll(byte data)
-        {
-
-        }
-
-        // $2006
-        public void WritePpuAddr(byte data)
-        {
-
-        }
-
-        // $2007
-        public void WritePpuData(byte data)
-        {
-
-        }
-
-        // $4014
-        public void WriteOamDma(byte data)
-        {
-
+            // допилить
         }
 
         // $2002
         public byte ReadPpuStatus()
         {
-
+            // допилить
         }
 
         // $2004
         public byte ReadOamData()
         {
+            return _oam[_oamAddr];
+        }
+        
+        // $2004
+        public void WriteOamData(byte data)
+        {
+            _oam[_oamAddr] = data;
+            _oamAddr++;
+        }
 
+        // $2005
+        public void WritePpuScroll(byte data)
+        {
+            // допилить
+        }
+
+        // $2006
+        public void WritePpuAddr(byte data)
+        {
+            // допилить
         }
 
         // $2007
         public byte ReadPpuData()
         {
+            // допилить
+        }
+        
+        // $2007
+        public void WritePpuData(byte data)
+        {
+            _memory.Write(v, data);
+            v += (ushort)(_vRamIncrement);
+        }
 
+        // $4014
+        public void WriteOamDma(byte data)
+        {
+            // допилить
+        }
+        
+        // $4014
+        public void WriteOamAddr(byte data)
+        {
+            _oamAddr = data;
         }
     }
 }
