0c1ffd4f81b803554a195658c20c40dabdd666d2
lievvl <37303364+lievvl@users.noreply.github.com>
2022-06-20 23:05:59 +0300

    feat: added Step emulating method, added incX/Y, fineY

--- PPU.cs
+++ PPU.cs
@@ -15,6 +15,15 @@ namespace GraphicProcessingUnit
         private int[] _spriteIndicies;
         private int _numSprites;
 
+        int Scanline;
+        int Cycle;
+
+        ulong _tileShiftReg;
+        byte _nameTableByte;
+        byte _attributeTableByte;
+        byte _tileBitfieldLo;
+        byte _tileBitfieldHi;
+
         ushort _baseNametableAddress;
         int _vRamIncrement;
         ushort _bgPatternTableAddress;
@@ -73,6 +82,78 @@ namespace GraphicProcessingUnit
             Array.Clear(_sprites, 0, _sprites.Length);
         }
 
+        public void Step()
+        {
+            // TODO: UpdateCounters();
+            
+            bool renderingEnabled = (_flagShowBackground != 0) || (_flagShowSprites != 0);
+            bool renderCycle = Cycle > 0 && Cycle <= 256;
+            bool preFetchCycle = Cycle >= 321 && Cycle <= 336;
+            bool fetchCycle = renderCycle || preFetchCycle;
+            
+            if (Scanline == 261 && Cycle == 1)
+            {
+                _nmiOccurred = 0;
+                // noindroid TODO: overflow = 0
+                // noindroid TODO: zeroHit = 0
+            }
+
+            if (renderingEnabled)
+            {
+                if (Cycle == 257)
+                {
+                    if (0 <= Scanline && Scanline <= 239) 
+                        //noindroid TODO: EvalSprites();
+                    else 
+                        _numSprites = 0;
+                }
+
+                if (renderCycle && (0 <= Scanline && Scanline <= 239))
+                    RenderPixel();
+
+                if (fetchCycle && ((0 <= Scanline && Scanline <= 239) || Scanline == 261))
+                {
+                    _tileShiftReg >>= 4;
+                    switch (Cycle % 8)
+                    {
+                        case 1:
+                            FetchNametableByte();
+                            break;
+                        case 3:
+                            FetchAttributeTableByte();
+                            break;
+                        case 5:
+                            // noindroid TODO: Fetch Bitfield Low
+                            break;
+                        case 7:
+                            // noindroid TODO: Fetch Bitfield High
+                            break;
+                        case 0:
+                            // noindroid TODO: save tile data
+                            IncrementX();
+                            if (Cycle == 256) 
+                                IncrementY();
+                            break;
+                    }
+
+                }
+                
+                if (Cycle > 257 && Cycle <= 320 && (Scanline == 261 || (0 <= Scanline && Scanline <= 239))) 
+                    _oamAddr = 0;
+                
+                if (Cycle == 257 && ((0 <= Scanline && Scanline <= 239) || Scanline == 261)) 
+                    CopyHorizPositionData();
+
+                if (Cycle >= 280 && Cycle <= 304 && Scanline == 261) 
+                    CopyVertPositionData();
+            }
+        }
+
+        void UpdateCounters()
+        {
+            // TODO: implement... PAMAGITE
+        }
+
         void CopyHorizontalData()
         {
             v = (ushort)((v & 0x7BE0) | (t & 0x041F));
@@ -88,9 +169,55 @@ namespace GraphicProcessingUnit
             return v & 0x001F;
         }
 
+        void IncrementX()
+        {
+            if ((v & 0x001F) == 31)
+            {
+                v = v & ~0x001F;
+                v = v ^ 0x0400;
+            }
+            else
+            {
+                v += 1;
+            }
+        }
+
         int CoarseY()
         {
             return (v & 0x03E0) >> 5;
+        }
+
+        void IncrementY()
+        {
+            if ((v & 0x7000) != 0x7000)
+            {
+                v += 0x1000;
+            }
+            else
+            {
+                v = v & ~0x7000;
+                int y = (v & 0x03E0) >> 5;
+                if (y == 29)
+                {
+                    y = 0;
+                    v = v ^ 0x0800;
+                }
+                else if (y == 31)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y += 1;
+                }
+            }
+
+            v = (v & ~0x03E0) | (y << 5);
+        }
+
+        int FineY()
+        {
+            return (v >> 12) & 0x7;
         }
 
         void FetchTileByte()
